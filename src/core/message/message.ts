bc64d2d5a6d4372c9e25ee3db07fd59b72449553
Jani Kangas <jnkngs@gmail.com>
2023-05-25 15:25:11 +0300

    message subscribe and unsubscribe

--- src/core/message/message.ts
+++ src/core/message/message.ts
@@ -1,4 +1,5 @@
-
+import {MessageBus} from "./messagebus";
+import {IMessageHandler} from "./imessagehandler"
 
 export enum MessagePriority {
     NORMAL,
@@ -20,5 +21,21 @@ export class Message {
         this.context = context;
         this.priority = priority;
 
+    }
+
+    public static send(code: string, sender: any, context?: any) {
+        MessageBus.post(new Message(code, sender, context, MessagePriority.NORMAL));
+    }
+
+    public static sendPriority(code: string, sender: any, context?: any) {
+        MessageBus.post(new Message(code, sender, context, MessagePriority.HIGH));
+    }
+
+    public static subscribe(code: string, handler: IMessageHandler) {
+        MessageBus.addSubscription(code, handler);
+    }
+
+    public static unsubscribe(code: string, handler: IMessageHandler) {
+        MessageBus.removeSubscription(code, handler);
     }
 }
--- src/core/message/messagebus.ts
+++ src/core/message/messagebus.ts
@@ -1,6 +1,67 @@
+import {IMessageHandler} from "./imessagehandler";
+import {MessageSubscriptionNode} from "./messagesubscriptionnode";
+import {Message, MessagePriority} from "./message";
 
 export class MessageBus {
-    
+
+    private static _subscriptions: {[code: string] : IMessageHandler[]} = {};
+
+    private static _nomalQueueMessagePerUpdate: number = 10;
+    private static _normalMessageQueue: MessageSubscriptionNode[] = []
     private constructor() {
+    }
+
+    public static addSubscription(code: string, handler: IMessageHandler) {
+        if(MessageBus._subscriptions[code] !== undefined) {
+            MessageBus._subscriptions[code] = [];
+        }
+
+        if(MessageBus._subscriptions[code].indexOf(handler) !== -1) {
+            console.warn(`Attempting to add duplicate handler to code ${code}. Subscription not added`);
+        } else {
+            MessageBus._subscriptions[code].push(handler);
+        }
+    }
+
+    public static removeSubscription(code: string, handler: IMessageHandler) {
+        if(MessageBus._subscriptions[code] === undefined) {
+            console.warn(`Cannot unsubscribe handler from code ${code} because that code is not subscribed to.`);
+            return;
+        }
+
+        let nodeIndex = MessageBus._subscriptions[code].indexOf(handler);
+
+        if(nodeIndex !== -1) {
+            MessageBus._subscriptions[code].splice(nodeIndex, 1);
+        }
+    }
+
+    public static post(message: Message) {
+        console.log("Message posted", message);
+
+        let handlers = MessageBus._subscriptions[message.code];
+        if(handlers === undefined) {
+            return;
+        }
+
+        for(let h of handlers) {
+            if(message.priority === MessagePriority.HIGH) {
+                h.onMessage(message);
+            } else {
+                MessageBus._normalMessageQueue.push(new MessageSubscriptionNode(message, h));
+            }
+        }
+    }
+
+    public static update(time: number) {
+        if(MessageBus._normalMessageQueue.length === 0) {
+            return;
+        }
+
+        let messageLimit = Math.min(MessageBus._nomalQueueMessagePerUpdate, MessageBus._normalMessageQueue.length);
+        for(let i = 0; i < messageLimit; ++i) {
+            let node = MessageBus._normalMessageQueue.pop();
+            node?.handler.onMessage(node?.message);
+        }
     }
 }
